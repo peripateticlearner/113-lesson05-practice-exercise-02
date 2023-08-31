41e03ddc06c005ccaefc52b1b4b1b67997838100
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2023-08-31 15:35:07 -0700

    Update script.js
    added variables, events, event listeners and event handlers

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var button = document.querySelector(".got-this");
+var modal = document.querySelector(".modal");
+var modalX = document.querySelector(".modal-x");
+//console.log(button, modal, modalX);
+
+button.addEventListener("click", function () {
+    modal.classList.add("show-modal");
+    button.innerText = "You've got this!!!";
+});
+
+modalX.addEventListener("click", function () {
+        modal.classList.remove("show-modal");
+        button.innerText = "Who's got this?";
+});
